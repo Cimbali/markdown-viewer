68d1de4dcde9a92366850b789b88ee01f87f11d8
Cimbali <me@cimba.li>
2020-08-08 17:06:54 +0200

    Fix longest line of CSS styles

--- ext/content.js
+++ ext/content.js
@@ -5,7 +5,21 @@ const headerTags = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6'];
 const pluginDefaults = {'hljs': true, 'checkbox': true, 'emojis': true, 'footnotes': false, 'fancy-lists': false};
 
 const mdcss = {'default': 'sss', 'github': 'github'}
-const hlcss = 'a11y-dark,a11y-light,agate,androidstudio,an-old-hope,arduino-light,arta,ascetic,atelier-cave-dark,atelier-cave-light,atelier-dune-dark,atelier-dune-light,atelier-estuary-dark,atelier-estuary-light,atelier-forest-dark,atelier-forest-light,atelier-heath-dark,atelier-heath-light,atelier-lakeside-dark,atelier-lakeside-light,atelier-plateau-dark,atelier-plateau-light,atelier-savanna-dark,atelier-savanna-light,atelier-seaside-dark,atelier-seaside-light,atelier-sulphurpool-dark,atelier-sulphurpool-light,atom-one-dark,atom-one-dark-reasonable,atom-one-light,brown-paper,brown-papersq,codepen-embed,color-brewer,darcula,dark,default,docco,dracula,far,foundation,github,github-gist,gml,googlecode,gradient-dark,grayscale,gruvbox-dark,gruvbox-light,hopscotch,hybrid,idea,ir-black,isbl-editor-dark,isbl-editor-light,kimbie,kimbie,lightfair,lioshi,magula,monokai,monokai-sublime,mono-blue,night-owl,nnfx,nnfx-dark,nord,obsidian,ocean,paraiso-dark,paraiso-light,pojoaque,pojoaque,purebasic,qtcreator_dark,qtcreator_light,railscasts,rainbow,routeros,school-book,school-book,shades-of-purple,solarized-dark,solarized-light,srcery,sunburst,tomorrow,tomorrow-night,tomorrow-night-blue,tomorrow-night-bright,tomorrow-night-eighties,vs,vs2015,xcode,xt256,zenburn'.split(',');
+const hlcss = ['a11y-dark', 'a11y-light', 'agate', 'androidstudio', 'an-old-hope', 'arduino-light', 'arta', 'ascetic',
+	'atelier-cave-dark', 'atelier-cave-light', 'atelier-dune-dark', 'atelier-dune-light', 'atelier-estuary-dark',
+	'atelier-estuary-light', 'atelier-forest-dark', 'atelier-forest-light', 'atelier-heath-dark', 'atelier-heath-light',
+	'atelier-lakeside-dark', 'atelier-lakeside-light', 'atelier-plateau-dark', 'atelier-plateau-light',
+	'atelier-savanna-dark', 'atelier-savanna-light', 'atelier-seaside-dark', 'atelier-seaside-light',
+	'atelier-sulphurpool-dark', 'atelier-sulphurpool-light', 'atom-one-dark', 'atom-one-dark-reasonable',
+	'atom-one-light', 'brown-paper', 'brown-papersq', 'codepen-embed', 'color-brewer', 'darcula', 'dark', 'default',
+	'docco', 'dracula', 'far', 'foundation', 'github', 'github-gist', 'gml', 'googlecode', 'gradient-dark', 'grayscale',
+	'gruvbox-dark', 'gruvbox-light', 'hopscotch', 'hybrid', 'idea', 'ir-black', 'isbl-editor-dark', 'isbl-editor-light',
+	'kimbie', 'kimbie', 'lightfair', 'lioshi', 'magula', 'monokai', 'monokai-sublime', 'mono-blue', 'night-owl', 'nnfx',
+	'nnfx-dark', 'nord', 'obsidian', 'ocean', 'paraiso-dark', 'paraiso-light', 'pojoaque', 'pojoaque', 'purebasic',
+	'qtcreator_dark', 'qtcreator_light', 'railscasts', 'rainbow', 'routeros', 'school-book', 'school-book',
+	'shades-of-purple', 'solarized-dark', 'solarized-light', 'srcery', 'sunburst', 'tomorrow', 'tomorrow-night', 'vs',
+	'tomorrow-night-blue', 'tomorrow-night-bright', 'tomorrow-night-eighties', 'vs2015', 'xcode', 'xt256', 'zenburn',
+]
 
 function addStylesheet(data) {
 	const style = document.createElement('style');
