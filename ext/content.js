f29fcd2121bac6f330e98f11bae331a28bfad6a6
KOLANICH <kolan_n@mail.ru>
2022-12-08 00:00:00 +0300

    Moved the code triggering insertion into the main document into `content.js`.

---
+++ ext/content.js
@@ -0,0 +1,11 @@
+﻿"use strict";
+
+// Process only if document is unprocessed text.
+const {body} = document;
+if (body.childNodes.length === 1 &&
+	body.children.length === 1 &&
+	body.children[0].nodeName.toUpperCase() === 'PRE')
+{
+	let placerLambda = (renderedDOM) => {body.replaceChild(renderedDOM, body.firstChild);}
+	renderIntoMainDoc(window.location.href, body.firstChild.textContent, placerLambda);
+}
--- ext/render.js
+++ ext/render.js
@@ -405,13 +405,3 @@ function renderIntoMainDoc(uri, source, placerLambda){
 	then(() => addMarkdownViewerMenu(document)).
 	then(() => createHTMLSourceBlob(document));
 }
-
-// Process only if document is unprocessed text.
-const {body} = document;
-if (body.childNodes.length === 1 &&
-	body.children.length === 1 &&
-	body.children[0].nodeName.toUpperCase() === 'PRE')
-{
-	let placerLambda = (renderedDOM) => {body.replaceChild(renderedDOM, body.firstChild);}
-	renderIntoMainDoc(window.location.href, body.firstChild.textContent, placerLambda);
-}
