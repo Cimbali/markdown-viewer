bceff521286ad28f57e6867a156a38f431f3147e
Cimbali <me@cimba.li>
2022-12-24 23:57:49 +0100

    Add ext+view-markdown: protocol calling our extension rendering page
    Note that this declaring of protocols is only supported by firefox.

--- ext/view-md.js
+++ ext/view-md.js
@@ -3,6 +3,9 @@
 function parseURI() {
 	let params = new URLSearchParams(window.location.search);
 	let url = new URL(params.get("file"));
+	if (url.protocol === 'ext+view-markdown:') {
+		url = new URL(url.href.slice(url.protocol.length));
+	}
 	return Promise.resolve(url);
 }
 
