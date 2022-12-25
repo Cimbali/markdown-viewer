28c29c446b77a7340694b0d2ccc4f61a058528ab
Cimbali <me@cimba.li>
2022-12-25 16:07:39 +0100

    Separate rendering from page building
    - Move rendering into its own class to prepare for workers
    - builder.js does not require all the markdownit scripts any more, only
      a “Renderer” class with a “render” function
    - renderer.js can do rendering given plugin prefs and text without all
      the page building logic

---
+++ ext/renderer.js
@@ -0,0 +1,51 @@
+'use strict';
+
+class Renderer {
+	constructor(plugins) {
+		this.plugins = plugins;
+	}
+
+	highlightCodeBlock(str, lang) {
+		// Shameless copypasta https://github.com/markdown-it/markdown-it#syntax-highlighting
+		if (lang && hljs.getLanguage(lang)) {
+			try {
+				return hljs.highlight(str, {language: lang}).value;
+			} catch (e) {}
+		}
+
+		try {
+			return hljs.highlightAuto(str).value;
+		} catch (e) {}
+		return ''; // use external default escaping
+	}
+
+	getRenderer(plugins) {
+		const md = markdownit({
+			html: true,
+			linkify: true,
+			...plugins.hljs ? {highlight: this.highlightCodeBlock} : {},
+		})
+		//markdown-it plugins:
+		if (plugins.checkbox) {md.use(markdownitCheckbox);}
+		if (plugins.emojis) {md.use(markdownitEmoji);}
+		if (plugins.footnotes) {md.use(markdownitFootnote);}
+		if (plugins.texmath) {
+			const tm = texmath.use(katex);
+			md.use(tm, {
+				engine: katex,
+				delimiters:'dollars',
+				katexOptions: { macros: {"\\RR": "\\mathbb{R}"} }
+			})
+		}
+		if (plugins['fancy-lists']) {
+			md.block.ruler.at('list', fancyList, { alt: [ 'paragraph', 'reference', 'blockquote' ] });
+		}
+
+		return md;
+	}
+
+	render(text) {
+		const html = this.getRenderer(this.plugins).render(text);
+		return Promise.resolve({ html });
+	}
+}
