d7cb445afc308a8468e2dd575e0f232d0e93fe38
Cimbali <me@cimba.li>
2022-12-27 00:19:39 +0100

    Add a browser action to access the local file prompt
    Accessible through toolbar button (hideable) and keyboard shortcut.
    Especially useful for users that can’t access the injected local page
    due to mime-type issues.
    
    Uses a background page, make it non-persistent (event-based).

--- ext/background.js
+++ ext/background.js
@@ -8,6 +8,10 @@ webext.runtime.onMessage.addListener((msg, sender) => {
 	}
 });
 
+webext.browserAction.onClicked.addListener(() => {
+	webext.tabs.create({ url: webext.runtime.getURL('/ext/view-md.html') });
+});
+
 webext.pageAction.onClicked.addListener((tab) => {
 	const dest = new URL(webext.runtime.getURL('/ext/view-md.html?file=') + encodeURIComponent(tab.url));
 	webext.tabs.update(tab.id, {url: dest.href}).then(tab => webext.pageAction.hide(tab.id)).catch(console.error);
