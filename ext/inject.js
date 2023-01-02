d76cf9672946bcfb3639727f0c90e858f92f9892
Cimbali <me@cimba.li>
2023-01-02 11:43:07 +0100

    Split content script in builder and inject scripts
    - builder builds the final page
    - inject is the part specific to on-page injection

---
+++ ext/inject.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+// NB. webext defined in builder.js
+//const webext = typeof browser === 'undefined' ? chrome : browser;
+
+// Process only if document is unprocessed text.
+const {body} = document;
+if (body.childNodes.length === 1 &&
+	body.children.length === 1 &&
+	body.children[0].nodeName.toUpperCase() === 'PRE')
+{
+	const inserter = renderedDOM => body.replaceChild(renderedDOM, body.firstChild);
+	renderDocument(body.firstChild.textContent, inserter, window.location.href)
+}
