c8a60d351360e7239c081510b339f2908175948e
Cimbali <me@cimba.li>
2023-01-15 18:12:24 +0100

    Watch URLs from background.js rather than manifest-based injection
    Centralises the permissions. Also remove unneeded "tabs" as we have host
    permissions for all the URLs we want.

--- ext/background.js
+++ ext/background.js
@@ -2,6 +2,11 @@
 
 const webext = typeof browser === 'undefined' ? chrome : browser;
 
+// Watch the updates of URLs we requested in the manifest
+const { permissions } = browser.runtime.getManifest();
+const urls = permissions.filter(perm => perm.includes('://'));
+
+// The list (and order) of scripts to render a page with injection
 const scripts = [
 	"lib/markdown-it/dist/markdown-it.min.js",
 	"lib/markdown-it-checkbox/dist/markdown-it-checkbox.min.js",
@@ -17,19 +22,54 @@ const scripts = [
 	"ext/inject.js"
 ];
 
+const checks = [
+	'document.body.childNodes.length === 1',
+	'document.body.children.length === 1',
+	'document.body.children[0].nodeName === "PRE"'
+].join(' && ');
+
+// Declare all event handlers at the top level
 webext.browserAction.onClicked.addListener(() => {
 	webext.tabs.create({ url: webext.runtime.getURL('/ext/view-md.html') });
 });
 
-webext.pageAction.onClicked.addListener(async (tab) => {
-	const url = new URL(tab.url);
-	const { redirect_local: redirect = false } = await webext.storage.sync.get('redirect_local');
-	if (url.protocol === 'file:' && !redirect) {
-		for (const path of scripts) {
-			await webext.tabs.executeScript(tab.id, { file: `/${path}` });
+function redirect(tabId, url) {
+	const dest = new URL(webext.runtime.getURL('/ext/view-md.html?file=') + encodeURIComponent(url));
+	webext.tabs.update(tabId, {url: dest.href}).catch(console.error);
+}
+
+async function tabUpdated(tabId, url, fallback=false) {
+	try {
+		const [isText] = await webext.tabs.executeScript(tabId, { code: checks });
+
+		if (!isText) {
+			return;
 		}
-	} else {
-		const dest = new URL(webext.runtime.getURL('/ext/view-md.html?file=') + encodeURIComponent(url));
-		webext.tabs.update(tab.id, {url: dest.href}).catch(console.error);
+	} catch (e) {
+		// Host permissions are likely not enabled
+		console.error(e);
+		if (fallback) {
+			redirect(tabId, url);
+		}
+		return;
 	}
-});
+
+	const { inject_local: inject = true } = await webext.storage.sync.get('inject_local');
+	if (url.protocol === 'file:' && inject) {
+		for (const path of scripts) {
+			await webext.tabs.executeScript(tabId, { file: `/${path}` });
+		}
+		webext.pageAction.hide(tabId);
+	} else {
+		// Default is to redirect to our page
+		redirect(tabId, url);
+	}
+}
+
+webext.pageAction.onClicked.addListener(tab => tabUpdated(tab.id, new URL(tab.url), true));
+
+browser.tabs.onUpdated.addListener((tabId, { 'status': update }, { url }) => {
+	if (update === 'complete') {
+		tabUpdated(tabId, new URL(url));
+	}
+}, { urls, properties: ['status'] });
