Cimbali/markdown-viewer

commit 6cb4c24c01d4902a446c68170b9564ed09d87457

Handle URLs without scheme
See #100

ext/view-md.js
@@ -6,16 +6,26 @@ function parseURI() {
 	if (file.startsWith('ext+view-markdown:')) {
 		file = file.slice('ext+view-markdown:'.length);
 	}
+
 	try {
-		return new URL(file);
-	} catch (e) {
-		try {
-			// Just a path?
-			return new URL(`file://${params.get("file")}`);
-		} catch (e) {
-			return null;
+		const url = new URL(file);
+		if (url.protocol) {
+			return url;
 		}
+	} catch (e) {}
+
+	// Try to handle URLs without schemes. Does it look like a local file path?
+	if (file.startsWith('/') || file.match(/^[A-Z]:/u)) {
+		try {
+			return new URL(`file://${file}`);
+		} catch (e) {}
+	} else {
+		try {
+			return new URL(`http://${file}`);
+		} catch (e) {}
 	}
+
+	return null;
 }
 
 const file = parseURI();

commit 8c2d4b5458bab74faaaca7d7c361922d6ee99d38

Handle absent and empty file params
Fixing regression from 6cb4c24

ext/view-md.js
@@ -3,6 +3,9 @@
 function parseURI() {
 	const params = new URLSearchParams(window.location.search);
 	let file = params.get("file");
+	if (!file) {
+		return null;
+	}
 	if (file.startsWith('ext+view-markdown:')) {
 		file = file.slice('ext+view-markdown:'.length);
 	}